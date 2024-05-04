ef142cb41eee1884a80f59b36becb68b391734de
HigorSantos <163744304+HigorPI@users.noreply.github.com>
2024-05-03 22:50:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Remova água parada de vasos de plantas, calhas, pneus velhos e outros recipientes ao redor de sua casa.
-        Mantenha piscinas limpas e bem tratadas.
-        Use repelente de insetos e roupas que cubram a maior parte do corpo.
-        Instale telas nas janelas e portas para impedir a entrada de mosquitos.
-        Use mosquiteiros em torno de camas e berços, especialmente durante o sono.'
+    return 'Higor'
+    
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Higor'
 
 
 if __name__ == '__main__':
